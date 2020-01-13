8c1c1a01effd057747fe71d08359a51bcc2fce87
Nurzhan Aitbayev <nurzhan@outlook.com>
2020-01-13 18:02:45 +0600

    Create DoublyLinkedList

---
+++ DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedList
+{
+    public class DoublyLinkedList<T> : ICollection<T>
+    {
+        public Node<T> Head { get; private set; }
+
+        public Node<T> Tail { get; private set; }
+
+        #region Add
+        
+        public void AddFirst(T value)
+        {
+            AddFirst(new Node<T>(value));
+        }
+
+        public void AddFirst(Node<T> node)
+        {
+            Node<T> temp = Head;
+
+            Head = node;
+
+            Head.Next = temp;
+
+            if (Count == 0)
+            {
+                Tail = Head;
+            }
+            else
+            {
+                temp.Previous = Head;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(T value)
+        {
+            AddLast(new Node<T>(value));
+        }
+
+        public void AddLast(Node<T> node)
+        {
+            if (Count == 0)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.Next = node;
+
+                node.Previous = Tail;
+            }
+
+            Tail = node;
+            Count++;
+        }
+        #endregion
+
+        #region Remove
+
+        public void RemoveFirst()
+        {
+            if (Count != 0)
+            {
+                Head = Head.Next;
+
+                Count--;
+
+                if (Count == 0)
+                {
+                    Tail = null;
+                }
+                else
+                {
+                    Head.Previous = null;
+                }
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (Count != 0)
+            {
+                if (Count == 1)
+                {
+                    Head = null;
+                    Tail = null;
+                }
+                else
+                {
+                    Tail.Previous.Next = null;
+                    Tail = Tail.Previous;
+                }
+
+                Count--;
+            }
+        }
+
+        #endregion
+
+        #region ICollection implementation
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            AddFirst(item);
+        }
+
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            Node<T> current = Head;
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Node<T> current = Head;
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            Node<T> previous = null;
+            Node<T> current = Head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                        {
+                            Tail = previous;
+                        }
+                        else
+                        {
+                            current.Next.Previous = previous;
+                        }
+
+                        Count--;
+                    }
+                    else
+                    {
+                        RemoveFirst();
+                    }
+
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public int Count { get; private set; }
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        #endregion
+    }
+}
